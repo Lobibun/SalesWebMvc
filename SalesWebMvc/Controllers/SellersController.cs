6d5b0b5388119e5029ef83d7cb598633a0195bcd
Alan Farias <alanfl885@gmail.com>
2025-12-15 11:20:15 -0300

    Adicionando SellerControler e link

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+
+
+        private readonly SalesWebMvcContext _context;
+
+        // 2. Construtor para injetar a dependência
+        public SellersController(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // 3. Buscar todos os vendedores do banco e passar para a View
+            var list = _context.Seller.ToList();
+            return View(list);
+        }
+    }
+}
